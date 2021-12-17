91847ae5ae6d3d6d055e601a3d4bc481ba2d58b1
enriquep5 <enriqueperez17@gmail.com>
2021-12-16 20:14:24 -0600

    first

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+/* function to help us retrieve form data
+-function to help us tell the user if they have invalid data in field (ie: letters in phone number)
+-prompt to congratulate user on successful appointment with appointment details ie:"Congratulations! You now have an appointment at 2pm on Saturday, December 18, 2021!"
+
+*/
